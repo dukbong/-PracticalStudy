2c93a1ac6d3277d3388700d0dac4e85496a7954e
janghs <gorae930301@gmail.com>
2024-02-07 15:58:03 +0900

    #36

--- demo/src/main/java/com/example/demo/controller/JenkinsTestController.java
+++ demo/src/main/java/com/example/demo/controller/JenkinsTestController.java
@@ -12,7 +12,7 @@ public class JenkinsTestController {
 	}
 	@GetMapping("/test")
 	public String test(){
-		return "TEST2 #35";
+		return "TEST2 #36";
 	}
 	@GetMapping("/home")
 	public String tset2(){
