8969ebf87aa4a78be76d5b26a131f9e3396cb822
janghs <gorae930301@gmail.com>
2024-02-07 16:14:29 +0900

    #37git add .

--- demo/src/main/java/com/example/demo/controller/JenkinsTestController.java
+++ demo/src/main/java/com/example/demo/controller/JenkinsTestController.java
@@ -12,7 +12,7 @@ public class JenkinsTestController {
 	}
 	@GetMapping("/test")
 	public String test(){
-		return "TEST2 #37!";
+		return "TEST2 #37!!";
 	}
 	@GetMapping("/home")
 	public String tset2(){
