f41047cab99aa540879e81571413b80de319a0dd
jang <gorae930301@gmail.com>
2024-02-08 15:08:00 +0900

    test

--- demo/src/main/java/com/example/demo/controller/JenkinsTestController.java
+++ demo/src/main/java/com/example/demo/controller/JenkinsTestController.java
@@ -12,7 +12,7 @@ public class JenkinsTestController {
 	}
 	@GetMapping("/test")
 	public String test(){
-		return "webhook test11~";
+		return "webhook!!!";
 	}
 	@GetMapping("/home")
 	public String tset2(){
